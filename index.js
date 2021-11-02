876b18c14a803e68405da60823e208a7242ffa55
Ahsan Habib Nahid <ahsanhabib3239@gmail.com>
2021-11-02 21:28:22 +0600

    crud

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId
+
+const app = express()
+const MongoClient = require('mongodb').MongoClient;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+const uri = "mongodb+srv://dbUser:y3lMikX1dQUftjw8@cluster0.e2ypf.mongodb.net/organicdb?retryWrites=true&w=majority";
+
+const password = 'y3lMikX1dQUftjw8'
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    const collection = client.db("organicdb").collection("products");
+    
+    //get data
+    app.get('/products', (req, res) => {
+        collection.find({})
+        .toArray((err, documents) => {
+            res.send(documents)
+        })
+    })
+
+    //post data
+    app.post('/addProduct', (req, res) => {
+        const product = req.body
+        collection.insertOne(product)
+        .then(result => {
+            console.log('data insert successfully')
+            res.send('data insert successfully')
+        })
+    })
+
+    // delete data
+    app.delete('/delete/:id', (req, res) => {
+        collection.deleteOne({_id: ObjectId(req.params.id)})
+        .then( result => {
+            console.log(result)
+        })
+    })
+
+    console.log('database connected')
+   
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(4000)
